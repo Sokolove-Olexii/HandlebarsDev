66412742c8b5a176df469f5210ec7a860660d6db
Sokolove-Olexii <130235909+Sokolove-Olexii@users.noreply.github.com>
2025-01-12 08:37:53 +0000

    Deploying to gh-pages from @ Sokolove-Olexii/HandlebarsDev@1b239cb2a83f8d44010f70c64b05861baa6d7597 🚀

---
+++ index.2c00dd2b.js
@@ -0,0 +1,2 @@
+template;const e=template({users:[{id:1,name:"Олександр",age:25},{id:2,name:"Марія",age:30},{id:3,name:"Іван",age:35}]});document.getElementById("app").innerHTML=e;
+//# sourceMappingURL=index.2c00dd2b.js.map
---
+++ index.6237d37a.js
@@ -0,0 +1,2 @@
+!function(){template;const e=template({users:[{id:1,name:"Олександр",age:25},{id:2,name:"Марія",age:30},{id:3,name:"Іван",age:35}]});document.getElementById("app").innerHTML=e}();
+//# sourceMappingURL=index.6237d37a.js.map
